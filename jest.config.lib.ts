9324b5daf08314b4c88e0e82b5ac5e5ea854a5c6
Devin Catuns <devincatuns1@gmail.com>
2024-08-22 21:04:36 -0400

    init next-starter plugin

---
+++ jest.config.lib.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'dcat23-nx',
+  preset: './jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/dcat23-nx',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,5 @@
-/* eslint-disable */
-export default {
-  displayName: 'dcat23-nx',
-  preset: './jest.preset.js',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: './coverage/dcat23-nx',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
-};
+import { getJestProjectsAsync } from '@nx/jest';
+
+export default async () => ({
+  projects: await getJestProjectsAsync(),
+});
