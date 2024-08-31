74c74452250d90f9a8deadaed451782d1ceed601
Devin Catuns <devincatuns1@gmail.com>
2024-08-31 16:53:45 -0400

    chore: update executors to publish with dcat23-nx:publish

--- src/executors/publish/executor.ts
+++ src/executors/publish/executor.ts
@@ -4,6 +4,10 @@ import { join } from "path";
 import { existsSync } from "fs";
 import { execSync } from "child_process";
 
+function resetVersion(packageJsonPath: string, packageJson: any, originalVersion: string) {
+
+}
+
 const runExecutor: PromiseExecutor<PublishExecutorSchema> = async (
   options,
   context
@@ -24,8 +28,8 @@ const runExecutor: PromiseExecutor<PublishExecutorSchema> = async (
 
   try {
     console.log(`Publishing ${projectRoot}@${packageJson.version}...`);
-    console.log(`command: npm publish ${options.distPath} --access public`)
     execSync(`npm publish ${options.distPath} --access public`, { stdio: 'inherit' });
+
     return { success: true };
   } catch (error) {
     console.error('Failed to publish:', error);
