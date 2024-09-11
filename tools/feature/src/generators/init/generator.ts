ab1fe01714ed68a014463784b56e8c65ec7f59bf
Devin Catuns <devincatuns1@gmail.com>
2024-09-11 11:59:54 -0400

    feat: added config and react query provider to feature init files

--- tools/feature/src/generators/init/generator.ts
+++ tools/feature/src/generators/init/generator.ts
@@ -1,11 +1,15 @@
 import {
+  addDependenciesToPackageJson,
   addProjectConfiguration,
   formatFiles,
   generateFiles,
-  Tree,
+  type GeneratorCallback, readJson, removeDependenciesFromPackageJson,
+  runTasksInSerial,
+  Tree, writeJson, writeJsonFile,
 } from '@nx/devkit';
 import * as path from 'path';
 import { InitGeneratorSchema } from './schema';
+import { latestVersion } from "./lib/versions";
 
 export async function initGenerator(tree: Tree, options: InitGeneratorSchema) {
   return initGeneratorInternal(tree, {
@@ -14,6 +18,49 @@ export async function initGenerator(tree: Tree, options: InitGeneratorSchema) {
     ...options
   });
 }
+
+function updateDependencies(host: Tree, schema: InitGeneratorSchema) {
+  const tasks: GeneratorCallback[] = [];
+  tasks.push(removeDependenciesFromPackageJson(host, ['@dcat23/nx-feature'], []));
+
+  tasks.push(
+    addDependenciesToPackageJson(
+      host,
+      {
+        '@tanstack/react-query': latestVersion,
+        'axios': latestVersion,
+        'zod': latestVersion,
+        'zustand': latestVersion,
+      },
+      {},
+      undefined,
+      schema.keepExistingVersions
+    )
+  );
+
+  return runTasksInSerial(...tasks);
+}
+
+function setDefaultTsConfig(tree: Tree, options: InitGeneratorSchema) {
+  const tsConfigPath = path.join(options.projectRoot, "tsconfig.json")
+
+  const tsConfig = readJson(tree, tsConfigPath) ?? {};
+
+  tsConfig["compilerOptions"] ??= {};
+  tsConfig["compilerOptions"]["baseUrl"] ??= options.projectRoot;
+  tsConfig["compilerOptions"]["paths"] ??= {};
+  tsConfig["compilerOptions"]["paths"]["@/*"] ??= [];
+
+  const srcPath = `${options.projectRoot}/src/*`;
+  const paths = tsConfig["compilerOptions"]["paths"]["@/*"] as string[]
+  if (!paths.includes(srcPath)) {
+    paths.push(srcPath);
+    tsConfig["compilerOptions"]["paths"]["@/*"] = paths;
+  }
+
+  writeJson(tree, tsConfigPath, tsConfig);
+}
+
 export async function initGeneratorInternal(tree: Tree, options: InitGeneratorSchema) {
   const projectRoot = options.projectRoot;
 
@@ -23,8 +70,22 @@ export async function initGeneratorInternal(tree: Tree, options: InitGeneratorSc
     sourceRoot: `${projectRoot}/src`,
     targets: {},
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
+  const templateOptions = {
+    ...options,
+    tmpl: ""
+  }
+
+  setDefaultTsConfig(tree, options)
+
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, templateOptions);
   await formatFiles(tree);
+  let installTask: GeneratorCallback = () => {};
+  if (!options.skipPackageJson) {
+    installTask = updateDependencies(tree, options);
+  }
+
+  return installTask;
 }
 
 export default initGenerator;
--- tools/feature/src/generators/init/schema.d.ts
+++ tools/feature/src/generators/init/schema.d.ts
@@ -1,4 +1,6 @@
 export interface InitGeneratorSchema {
   projectRoot: string;
   name: string;
+  skipPackageJson: boolean;
+  keepExistingVersions: boolean;
 }
